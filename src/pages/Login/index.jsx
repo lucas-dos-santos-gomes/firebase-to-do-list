60977a234dbe71ee25c70d3475f40ee3e97c8935
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-06-14 17:12:29 -0300

    Login complete

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+import { auth } from "../../contexts/auth";
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleAuth = async(e) => {
+    e.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch(err) {
+      setError(err.message);
+    }
+};
+
   return (
     <div>
       <main>
         <img src="" alt="" />
         <h1>Entre com sua conta</h1>
-        <form>
-          <input id="email" type="email" placeholder="E-mail" />
-          <input id="password" type="password" placeholder="Senha" minLength={8} />
+        <form onSubmit={handleAuth}>
+          <input id="email" type="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} maxLength={50} required />
+          <input id="password" type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} minLength={8} maxLength={24} required />
           <button type="submit">Entrar</button>
         </form>
+        {error && <p>{error}</p>}
       </main>
     </div>
   );
