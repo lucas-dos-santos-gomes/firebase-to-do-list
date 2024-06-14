c6c69853d9beb33360df4fe7fdff6aeedcb69ae4
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-06-14 17:26:45 -0300

    Logout complete

--- src/pages/Tasks/index.jsx
+++ src/pages/Tasks/index.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+import { auth } from "../../contexts/auth";
+
 export default function Tasks() {
+  const [seconds, setSeconds] = useState(5);
+
+  const handleLogout = async() => {
+    if(seconds > 0) {
+      await new Promise(setTimeout(() => setSeconds(seconds-1), 1000));
+    } else {
+      auth.signOut();
+    }
+  }
+
+  useEffect(() => {
+    handleLogout();
+  });
+
   return (
-    <>Tasks</>
+    <>Tasks {seconds}</>
   );
 }
