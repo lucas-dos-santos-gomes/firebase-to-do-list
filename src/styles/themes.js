088fe8a0e6bd8ef9b35ccbb9a6c8eb7952833b76
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-06-16 19:33:46 -0300

    Theme defined

---
+++ src/styles/themes.js
@@ -0,0 +1,27 @@
+export const themes = {
+  color: {
+    blue: '#4095eb',
+  },
+  light: {
+    color : {
+      primary: '#AEAEAE',
+      secondary: '#FFF',
+      tertiary: '#F4F4F4',
+    },
+    fontColor : {
+      primary: '#242424',
+      placeholder: '#9D9D9D',
+    },
+  },
+  dark: {
+    color: {
+      primary: '#242424',
+      secondary: '#3A3A3A',
+      tertiary: '#595959',
+    },
+    fontColor: {
+      primary: '#FFF',
+      placeholder: '#B0B0B0',
+    },
+  },
+};
